8d42f9ed6c318d417233cab8960a73525fc08455
Michalina-Dziwinska <83969041+Michalina-Dziwinska@users.noreply.github.com>
2021-05-23 10:47:48 +0200

    Update zapytanie_2_MR.js

--- zapytanie_2_MR.js
+++ zapytanie_2_MR.js
@@ -1,13 +1,17 @@
 var mapFcn2 = function() {
 	for (i = 0; i < this.credit.length; i++){
 		emit(this.credit[i].currency, parseFloat(this.credit[i].balance));}};
-var redFcn2 = function(curreny, balance) {
-	balance = 0;
+var reduceFcn2 = function(curreny, balance) {
+	totalBalanceLeft = 0;
 	for (i = 0; i < balance.length; i++){
-		balance += balance[i];}
-	return balance;}
+		totalBalanceLeft += balance[i];}
+	return totalBalanceLeft;}
 db.people.mapReduce(
-	mapFcn2,redFcn2,
-	{out: "map_2",})
+	mapFcn2,
+	reduceFcn2,
+	{
+		out: "map_2",
+	}
+)
 
 printjson(db.map_2.find().toArray())
